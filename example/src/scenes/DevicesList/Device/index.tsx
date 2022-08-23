ca4e5238243202e11d317cbb35d48eeea26fcfce
Solo <juan@xmartlabs.com>
2022-08-23 16:23:08 -0300

    splits ios and android interfaces

--- example/src/scenes/DevicesList/Device/index.tsx
+++ example/src/scenes/DevicesList/Device/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Platform, Text, TouchableOpacity, View } from 'react-native';
-import {
-  provision,
-  provisionAndroid,
-  scanWifi,
-  scanWifiAndroid,
-} from '../../../../../src/index';
+import { android, ios } from '../../../../../src/index';
 import type { BleDevice } from 'src/entities';
 
 import { styles } from './styles';
@@ -14,28 +9,27 @@ interface Props {
   device: BleDevice;
 }
 
-const SSID = 'Xmartlabs';
-const PASS_PHRASE = 'Xmartlabs33';
+const SSID = 'MySsid';
+const PASS_PHRASE = 'MyPassPhrase';
 
 export const Device: React.FC<Props> = (props) => {
   const onPress = async () => {
-    console.log('## onPress START');
     let wifiList = [];
     if (Platform.OS === 'ios') {
-      wifiList = await scanWifi(props.device);
+      wifiList = await ios.scanWifi(props.device);
     } else {
-      console.log('## scanWifiAndroid START');
-      wifiList = await scanWifiAndroid(props.device.uuid, props.device.address);
-      console.log('## wifiList: ', wifiList);
+      wifiList = await android.scanWifi(
+        props.device.uuid,
+        props.device.address
+      );
     }
 
-    const provisioningStatus = await provisionAndroid(
+    const provisioningStatus = await android.provision(
       props.device.uuid,
       props.device.address,
       SSID,
       PASS_PHRASE
     );
-    console.log('## provisioningStatus: ', provisioningStatus);
   };
 
   return (
